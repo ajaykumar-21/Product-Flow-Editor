6ef21fd7f37aba02a25f8ec6474bf6d63e8097e9
ajaykumar-21 <ajaykumarpk789@gmail.com>
2025-04-24 23:27:11 +0530

    using grid make to portion one product display and other for flow editor

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import FlowEditor from "./components/FlowEditor";
 
 function App() {
   return (
-    <div>
-      <ProductList />
-      <FlowEditor />
+    <div className="flex flex-col md:flex-row h-screen">
+      <div className="w-full md:w-1/2 p-4 border-r overflow-y-auto">
+        <ProductList />
+      </div>
+      <div className="w-full md:w-2/3 p-4">
+        <FlowEditor />
+      </div>
     </div>
   );
 }
