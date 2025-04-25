26e8900128b0a0b48ead83ee30b244720ac2ad84
ajaykumar-21 <ajaykumarpk789@gmail.com>
2025-04-25 16:38:56 +0530

    add onChangeNode and Edge when node and edges make changes

--- src/components/FlowEditor.js
+++ src/components/FlowEditor.js
@@ -1,17 +1,42 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import ReactFlow, {
   MiniMap,
   Controls,
   Background,
+  applyNodeChanges,
+  applyEdgeChanges,
 } from "reactflow";
 
 function FlowEditor() {
+  const [nodes, setnodes] = useState([]);
+  const [edges, setEdges] = useState([]);
+
+  const onNodesChange = useCallback(
+    (changes) => {
+      setnodes((nds) => applyNodeChanges(changes, nds));
+    },
+    [setnodes]
+  );
+
+  const onEdgesChange = useCallback(
+    (changes) => {
+      setEdges((eds) => applyEdgeChanges(changes, eds));
+    },
+    [setEdges]
+  );
+
   return (
     <div className="w-full h-full border rounded shadow">
-      <ReactFlow>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        fitView
+      >
         <MiniMap />
         <Controls />
-        <Background variant="dots" gap={12} size={1} />
+        <Background variant="dots" gap={10} size={1} />
       </ReactFlow>
     </div>
   );
